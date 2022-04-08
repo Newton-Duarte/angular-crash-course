ad4b49f83f6b7f57ab229943e4c1d76fb3dc8af3
Newton Duarte <newton_@hotmail.com>
2022-04-08 06:16:33 -0300

    feat: add crud operations for tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -21,4 +21,20 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
   }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+
+    return this.http.put<Task>(url, task, httpOptions);
+  }
 }
