528faad14b86c22e71dabc139363aeeef69a5629
Newton Duarte <newton_@hotmail.com>
2022-04-08 06:16:17 -0300

    feat: add http options with headers object

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,12 @@ import { Observable } from 'rxjs';
 
 import { Task } from '../Task';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
